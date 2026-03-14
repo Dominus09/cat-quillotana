5d4897368f237848440ec0a84139c39b0ab565ba
Dominus09 <katoonxd@gmail.com>
2026-03-14 20:53:24 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com"
+      }
+    ]
+  }
 }
 
-module.exports = nextConfig
+export default nextConfig
